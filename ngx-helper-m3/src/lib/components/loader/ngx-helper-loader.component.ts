a34551bc7763ecd98511c9b4f56acaeff63f1954
Ali Amirnezhad <webilix@gmail.com>
2024-12-24 01:19:24 +0330

    Update loader component

--- ngx-helper-m3/src/lib/components/loader/ngx-helper-loader.component.ts
+++ ngx-helper-m3/src/lib/components/loader/ngx-helper-loader.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, HostBinding, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
     selector: 'ngx-helper-loader',
@@ -6,16 +6,25 @@ import { Component, HostBinding, Input, OnChanges, SimpleChanges } from '@angula
     templateUrl: './ngx-helper-loader.component.html',
     styleUrl: './ngx-helper-loader.component.scss',
 })
-export class NgxHelperLoaderComponent implements OnChanges {
+export class NgxHelperLoaderComponent implements OnInit, OnChanges {
     @HostBinding('style.--loader-size') public loaderSize!: string;
     @HostBinding('style.--loader-color') public loaderColor!: string;
 
-    @Input({ required: true }) public mode!: 'SPINNER' | 'DOTS-SPINNER' | 'WHEEL-SPINNER';
+    @Input({ required: false }) public mode: 'SPINNER' | 'DOTS-SPINNER' | 'WHEEL-SPINNER' = 'SPINNER';
     @Input({ required: false }) public size!: number;
     @Input({ required: false }) public color!: string;
     @Input({ required: false }) public padding?: string;
+    @Input({ required: false }) public margin?: string;
+
+    ngOnInit(): void {
+        this.init();
+    }
 
     ngOnChanges(changes: SimpleChanges): void {
+        this.init();
+    }
+
+    init(): void {
         this.loaderSize = `${this.size || 25}px`;
         this.loaderColor = this.color || `var(--secondary)`;
     }
